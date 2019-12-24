4bc16db7bb7ae7df8fc839d3972a0b72670be1b9
chenxi <chenxi@bitnei.cn>
2019-12-24 16:18:27 +0800

    viewer opyimize

--- src/main/java/graph/graphstream/GraphStream.java
+++ src/main/java/graph/graphstream/GraphStream.java
@@ -3,8 +3,10 @@ package graph.graphstream;
 import com.github.abel533.echarts.axis.CategoryAxis;
 import com.github.abel533.echarts.axis.ValueAxis;
 import com.github.abel533.echarts.code.Magic;
+import com.github.abel533.echarts.code.MarkType;
 import com.github.abel533.echarts.code.Tool;
 import com.github.abel533.echarts.code.Trigger;
+import com.github.abel533.echarts.data.PointData;
 import com.github.abel533.echarts.feature.MagicType;
 import com.github.abel533.echarts.series.Bar;
 import compute.constractor.GraphBuilder;
@@ -54,32 +56,32 @@ public class GraphStream {
         //graph.display();
 
         EnhancedOption option = new EnhancedOption();
-        option.title().text("Entropy").subtext("各结构熵对比分析");
+        option.title().text("Entropy").subtext("Comparison result");
         option.tooltip().trigger(Trigger.axis);
-        option.legend("SD熵");
+        option.legend("Classic_Topology_Entropy","DD_Entropy","WU_Entropy","SD_Entropy");
         option.toolbox().show(true).feature(Tool.mark, Tool.dataView, new MagicType(Magic.line, Magic.bar).show(true), Tool.restore, Tool.saveAsImage);
         option.calculable(true);
-        option.xAxis(new CategoryAxis().data("简单", "中等", "复杂", "及其复杂"));
+        option.xAxis(new CategoryAxis().data("easy", "medal", "hard", "complex"));
         option.yAxis(new ValueAxis());
 
-        Bar bar1 = new Bar("classic");
+        Bar bar1 = new Bar("Classic_Topology_Entropy");
         bar1.data(compute1.classicEntropyInfo(), compute2.classicEntropyInfo(), compute3.classicEntropyInfo(), compute4.classicEntropyInfo());
         //bar.markPoint().data(new PointData().type(MarkType.max).name("最大值"), new PointData().type(MarkType.min).name("最小值"));
         //bar1.markLine().data(new PointData().type(MarkType.average).name("平均值"));
 
-        Bar bar2 = new Bar("DDEntropy");
+        Bar bar2 = new Bar("DD_Entropy");
         bar2.data(compute1.getDDEntropy(true), compute2.getDDEntropy(true), compute3.getDDEntropy(true), compute4.getDDEntropy(true));
         //bar.markPoint().data(new PointData().type(MarkType.max).name("最大值"), new PointData().type(MarkType.min).name("最小值"));
         //bar2.markLine().data(new PointData().type(MarkType.average).name("平均值"));
 
-        Bar bar3 = new Bar("WUEntropy");
+        Bar bar3 = new Bar("WU_Entropy");
         bar3.data(compute1.getWUEntropy(), compute2.getWUEntropy(), compute3.getWUEntropy(), compute4.getWUEntropy());
         //bar.markPoint().data(new PointData().type(MarkType.max).name("最大值"), new PointData().type(MarkType.min).name("最小值"));
         //bar3.markLine().data(new PointData().type(MarkType.average).name("平均值"));
 
-        Bar bar4 = new Bar("SDEntropy");
+        Bar bar4 = new Bar("SD_Entropy");
         bar4.data(compute1.getSDEntropy(), compute2.getSDEntropy(), compute3.getSDEntropy(), compute4.getSDEntropy());
-        //bar.markPoint().data(new PointData().type(MarkType.max).name("最大值"), new PointData().type(MarkType.min).name("最小值"));
+        //bar4.markPoint().data(new PointData().type(MarkType.max).name("最大值"), new PointData().type(MarkType.min).name("最小值"));
         //bar4.markLine().data(new PointData().type(MarkType.average).name("平均值"));
 
         option.series(bar1, bar2, bar3, bar4);
